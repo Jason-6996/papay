008a68f4115cbd82097386735a4de34ee5267bc0
Jason-6996 <umidjon95@gmail.com>
2023-05-28 16:05:41 +0900

    fix: mongoose connection va member schema model qurish

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ const express = require("express");
 const app = express();
 const router = require("./router");
 
-//MongoDB connect
-const db = require("./server").db();
-const mongodb = require("mongodb");
 
 // 1: Kirish code
 // express ga kirib kelayotgan kodlar yoziladi
--- models/Member.js
+++ models/Member.js
@@ -1,7 +1,5 @@
 class Member {
-  constructor() {
-
-  }
+  constructor() { }
 }
 
 
