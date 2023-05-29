2f95134caa2420e9fecac0ed78794f0f239d9a3a
Jason-6996 <umidjon95@gmail.com>
2023-05-29 14:04:52 +0900

    fix: membermodel.js some of the errors have been fixed

--- schema/member.model.js
+++ schema/member.model.js
@@ -7,65 +7,6 @@ const memberSchema = new mongoose.Schema({
     required: true,
     index: { unique: true, sparse: true }
   },
-  mb_phone: {
-    type: String,
-    required: true,
-  },
-  mb_password: {
-    type: String,
-    required: true,
-    select: false
-  },
-  mb_type: {
-    type: String,
-    required: false,
-    default: "USER",
-    enum: {
-      values: member_type_enums,
-      message: "{VALUE} is not among permitted values"
-    }
-  },
-
-  mb_status: {
-    type: String,
-    required: false,
-    default: "ACTIVE",
-    enum: {
-      values: member_status_enums,
-      message: "{VALUE} is not among permitted values"
-    }
-  },
-  mb_address: {
-    type: String,
-    required: false,
-  },
-  mb_description: {
-    type: String,
-    required: false,
-  },
-  mb_image: {
-    type: String,
-    required: false,
-  },
-  mb_point: {
-    type: Number,
-    required: false,
-    default: 0
-  },
-  mb_top: {
-    type: String,
-    required: false,
-    default: "N",
-    enum: {
-      values: ordinary_enums,
-      message: "{VALUE} is not among permitted values"
-    }
-  },
-  mb_views: {
-    type: Number,
-    required: false,
-    default: 0
-  },
   mb_views: {
     type: Number,
     required: false,
@@ -86,7 +27,8 @@ const memberSchema = new mongoose.Schema({
     required: false,
     default: 0
   },
+},
   { timestamps: true } // mongodb avtomatik tarzda 2 xil qiymat beradi createdAt va updatedAt ni shu method asosida
-});
+);
 
 module.exports = mongoose.model("Member", memberSchema);
