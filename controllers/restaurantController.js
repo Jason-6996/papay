aa8d343c23d66b4d92829d42a4ca7e43119e7b70
Jason-6996 <umidjon95@gmail.com>
2023-06-24 04:53:10 +0900

    fix: complete development of allrestaurant business logic

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -136,10 +136,8 @@ restaurantController.validateAdmin = (req, res, next) => {
 restaurantController.getAllRestaurants = async (req, res) => {
   try {
     console.log("GET cont/getAllRestaurants");
-
     const restaurant = new Restaurant();
     const restaurants_data = await restaurant.getAllRestaurantsData();
-    console.log("restaurant_data:", restaurants_data);
     res.render("all-restaurants", { restaurants_data: restaurants_data });
   } catch (err) {
     console.log(`ERROR, cont/getAllRestaurants, ${err.message}`);
@@ -150,7 +148,6 @@ restaurantController.getAllRestaurants = async (req, res) => {
 restaurantController.updateRestaurantByAdmin = async (req, res) => {
   try {
     console.log("GET cont/updateRestaurantByAdmin");
-
     const restaurant = new Restaurant();
     const result = await restaurant.updateRestaurantByAdminData(req.body);
     await res.json({ state: "success", data: result });
